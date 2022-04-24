ccb460d61fe589eb82aaa513dc3ae802592a42f5
Darshan Mehar <darshanmehar304@gmail.com>
2022-04-25 02:58:58 +0530

    new update

--- app/src/main/java/com/example/darshanambulance/MainActivity.java
+++ app/src/main/java/com/example/darshanambulance/MainActivity.java
@@ -21,23 +21,14 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button register,login;
-    private EditText et_username, et_age,et_email,et_password;
     private ProgressBar progressBar;
-    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         register = (Button) findViewById(R.id.register_btn);
         login = (Button) findViewById(R.id.login_btn);
-        et_username = (EditText) findViewById(R.id.user_name);
-        et_age = (EditText) findViewById(R.id.user_age);
-        et_email = (EditText) findViewById(R.id.user_email);
-        et_password = (EditText) findViewById(R.id.user_password);
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
         register.setOnClickListener(this);
         login.setOnClickListener(this);
     }
@@ -46,78 +37,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.register_btn:
-                 registerUser();
+                Intent rintent = new Intent(MainActivity.this, RegisterUser.class);
+                startActivity(rintent);
                  break;
             case R.id.login_btn:
-                Intent intent = new Intent(MainActivity.this, LoginUser.class);
-                startActivity(intent);
+                Intent lintent = new Intent(MainActivity.this, LoginUser.class);
+                startActivity(lintent);
                 break;
         }
     }
 
-    private void registerUser() {
-        String username = et_username.getText().toString().trim();
-        String age = et_age.getText().toString().trim();
-        String email = et_email.getText().toString().trim();
-        String password = et_password.getText().toString().trim();
-
-        if(username.isEmpty()){
-            et_username.setError("Please Enter Username");
-            et_username.requestFocus();
-            return;
-        }
-        if(username.isEmpty()){
-            et_age.setError("Please Enter Your Age");
-            et_age.requestFocus();
-            return;
-        }
-
-        if(email.isEmpty()){
-            et_email.setError("Please Enter Email Address");
-            et_email.requestFocus();
-            return;
-        }
-
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            et_email.setError("Please Enter Valid Email Address");
-            et_email.requestFocus();
-            return;
-        }
-
-        if(password.isEmpty()){
-            et_password.setError("Please Enter Password");
-            et_password.requestFocus();
-            return;
-        }
-
-        if(password.length() < 6){
-            et_password.setError("Min Password lenght should be 6 characters");
-            et_password.requestFocus();
-            return;
-        }
-
-        progressBar.setVisibility(View.VISIBLE);
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Users users = new Users(username,age,email);
-                    FirebaseDatabase.getInstance().getReference("Users")
-                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                            .setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(MainActivity.this,"User has been registered successfully !",Toast.LENGTH_LONG).show();
-                                progressBar.setVisibility(View.GONE);
-                            }else{
-                                Toast.makeText(MainActivity.this,"Failed to registered try gain", Toast.LENGTH_LONG).show();
-                                progressBar.setVisibility(View.GONE);
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    }
 }
